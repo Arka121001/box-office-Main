efce1c8158e1a08694dd88ceb1b5ac17665f9d51
Arkaprabha <arkaprabha1234567@gmail.com>
2021-07-08 01:38:48 +0530

    The Navigation and Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import {Switch , Route} from 'react-router-dom'
+import  Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred'
 
 function App() {
   return (
+  <div>
+
+    <Navs/>
     <Switch>
 
-      <Route exact path = "/">This is home page </Route>
+      <Route exact path = "/"> <Home/> </Route>
 
-      <Route  exact path = "/starred">This is home starred </Route>
+      <Route  exact path = "/starred"> <Starred/> </Route>
 
-      <Route> This is error page </Route>
-
-
+      <Route> <div> NOT Found </div> </Route>
 
     </Switch>
+    </div>
   );
 }
 
