d35d53a0af0384077c4f0fd662b4cc9fe2f534fc
Arkaprabha <arkaprabha1234567@gmail.com>
2021-07-08 21:54:08 +0530

    The Search Bar Done with In keyboard Enter key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
+
+  const [input, setInput ] = useState('');
+
+  const onSearch = () => {
+
+    // https://api.tvmaze.com/search/shows?q=men
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json())
+    .then(result => {
+
+    console.log(result);
+    })
+
+  };
+
+
+  const onInputChange = ev => {
+
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = (ev) => {
+    if(ev.keyCode === 13){
+      onSearch()
+    }
+    
+  };
+
+
   return ( 
     <MainPageLayout>
-      this is Home
+      <input type = "text" onChange = {onInputChange} onKeyDown = {onKeyDown}value = {input}/>
+      <button type = "button" on onClick={onSearch}>Search</button>
     </MainPageLayout>
 
   )
